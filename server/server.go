d726068e9cbadbf1ada1bbd0068717af8bce95ae
vvv112 <vasily@dudinsky.ru>
2021-11-17 19:40:23 +0300

    server 0.0.1 test

--- server/server.go
+++ server/server.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	)
+
 func main() {
 	println("server 0.0.1")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "server1")
+		})
+		http.ListenAndServe(":80", nil)
+
 }
